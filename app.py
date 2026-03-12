96167db2c7131ab125bfc4b29dcbeff5692a2e04
renukadagada <renukadagada@gmail.com>
2026-03-12 21:46:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,19 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model = joblib.load("fitness_model.pkl")
+st.title("AI Health and Fitness Assistant")
 
-st.title("AI Health & Fitness Assistant")
+# Load model
+model = joblib.load("fitness_model.pkl")
 
 age = st.number_input("Age")
 height = st.number_input("Height (cm)")
 weight = st.number_input("Weight (kg)")
-duration = st.number_input("Exercise Duration (minutes)")
+duration = st.number_input("Workout Duration (minutes)")
 heart_rate = st.number_input("Heart Rate")
 
 if st.button("Predict Calories Burned"):
-    
     data = np.array([[age, height, weight, duration, heart_rate]])
-    
     prediction = model.predict(data)
-    
+
     st.success(f"Estimated Calories Burned: {prediction[0]:.2f}")
